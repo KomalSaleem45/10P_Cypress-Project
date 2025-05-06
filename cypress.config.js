225c739151adb4eb1f875f878260f2ffbdd8b855
Komal Saleem <148528742+KomalSaleem45@users.noreply.github.com>
2025-05-06 23:46:41 +0500

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    setupNodeEvents(on) {
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
